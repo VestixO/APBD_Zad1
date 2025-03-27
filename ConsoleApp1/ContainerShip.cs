a9c31146554e76899c2b01593907b06f5c079814
VestixO <s28657@pjwstk.edu.pl>
2025-03-27 21:25:19 +0100

    Initial commit

---
+++ ConsoleApp1/ContainerShip.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp1;
+using System;
+using System.Collections.Generic;
+
+
+class ContainerShip
+{
+    private List<Container> Containers = new List<Container>();
+    public double MaxWeight { get; }
+    public int MaxContainers { get; }
+
+    public ContainerShip(double maxWeight, int maxContainers)
+    {
+        MaxWeight = maxWeight;
+        MaxContainers = maxContainers;
+    }
+
+    public void LoadContainer(Container container)
+    {
+        if (Containers.Count >= MaxContainers)
+            throw new Exception("Statek nie może przyjąć więcej kontenerów!");
+
+        Containers.Add(container);
+    }
+
+    public void PrintContainers()
+    {
+        Console.WriteLine("Statek przewozi kontenery:");
+        foreach (var container in Containers)
+        {
+            Console.WriteLine($"Serial: {container.SerialNumber}, Obecny ładunek: {container.GetCurrentLoad()}/{container.MaxLoad} kg");
+        }
+    }
+}
---
+++ ConsoleApp1/GasContainer.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp1;
+using System;
+
+
+class GasContainer : Container
+{
+    public double Pressure { get; }
+
+    public GasContainer(double maxLoad, double pressure) : base(maxLoad, "G")
+    {
+        Pressure = pressure;
+    }
+
+    public override void Unload()
+    {
+        CurrentLoad *= 0.05;
+    }
+}
